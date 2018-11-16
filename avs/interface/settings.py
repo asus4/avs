90f0b20f5f583d95f214d46397e1b7a9ff6bd170
Koki Ibukuro <koki.ibukuro@gmail.com>
2018-11-16 11:20:50 +0100

    Fix indent

--- avs/interface/settings.py
+++ avs/interface/settings.py
@@ -9,24 +9,24 @@ class Settings(object):
     def __init__(self, alexa):
         self.alexa = alexa
     
-    def SetLocale(locale):
-         event = {
-        "event": {
-            "header": {
-                "namespace": "Settings",
-                "name": "SettingsUpdated",
-                "messageId": uuid.uuid4().hex
-            },
-            "payload": {
-                "settings": [
-                    {
-                        "key": "locale",
-                        "value": locale
-                    }
-                ]
+    def SetLocale(self, locale):
+        event = {
+            "event": {
+                "header": {
+                    "namespace": "Settings",
+                    "name": "SettingsUpdated",
+                    "messageId": uuid.uuid4().hex
+                },
+                "payload": {
+                    "settings": [
+                        {
+                            "key": "locale",
+                            "value": locale
+                        }
+                    ]
+                }
             }
         }
-    }
         self.alexa.send_event(event)
 
     @property
